6daa3fd8a5bf9ea1ea426ff1c1b296e3273db1dc
Yassine Gherbi <y.gherbi.dev@gmail.com>
2023-03-18 02:48:18 +0100

    add answers for chapter 3

--- src/03-unions-intersections/02-intersection-types.ts
+++ src/03-unions-intersections/02-intersection-types.ts
@@ -1,30 +1,29 @@
 // use intersection types to convert the timestamp into another object
 
 type BirthdayFromBackend = {
-  epochMs: number;
-};
+  epochMs: number
+}
 
-const albertsBirthday = {
+const elonMusksBirthdayData = {
   epochMs: 652602938824,
-};
+}
 
 // create a type for a function that "extends" the Birthday type to a Date
 type BirthdayFromBackendWithDate = BirthdayFromBackend & {
-  jsDate: Date;
+  jsDate: Date
 }
 
-
-type ToDate = (birthday: BirthdayFromBackend) => BirthdayFromBackendWithDate;
+type ToDate = (birthday: BirthdayFromBackend) => BirthdayFromBackendWithDate
 
 // implement the function that uses this type
-const calculateDateFromEpochMs: ToDate = (albertsBirthday) => {
+const calculateDateFromEpochMs: ToDate = elonMusksBirthday => {
   return {
-    ...albertsBirthday,
-    jsDate: new Date(albertsBirthday.epochMs),
-  };
+    ...elonMusksBirthday,
+    jsDate: new Date(elonMusksBirthday.epochMs),
+  }
 }
 
 // ===============================
 // Don't change below these lines
 // ===============================
-console.log(calculateDateFromEpochMs(albertsBirthday).jsDate.getUTCDate());
+console.log(calculateDateFromEpochMs(elonMusksBirthdayData).jsDate.getUTCDate())
