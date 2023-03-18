f356d05746a1d10266ce7b75fcbc724f5b606859
Yassine Gherbi <y.gherbi.dev@gmail.com>
2023-03-18 02:58:09 +0100

    add answers for chapter 05

--- src/05-top-bottom-types/01-top-any-unknown.ts
+++ src/05-top-bottom-types/01-top-any-unknown.ts
@@ -1,17 +1,17 @@
 // fix the following types
 
 // answer
-type ParamA = undefined;
-type ParamB = undefined;
+type ParamA = { a: { b: { c: { d: { e: string } } } } }
+type ParamB = { a: { b: { c: { d: { e: number } } } } }
 
 // ===============================
 // Don't change below these lines
 // ===============================
 function SomeFunction(a: ParamA) {
-  return a.a.b.c.d.e;
+  return a.a.b.c.d.e
 }
 
-function AnotherFunction(b: ParamB) : string {
+function AnotherFunction(b: ParamB): string {
   //@ts-expect-error
-  return b.a.b.c.d.e;
+  return b.a.b.c.d.e
 }
--- src/05-top-bottom-types/02-bottom-never.ts
+++ src/05-top-bottom-types/02-bottom-never.ts
@@ -1,24 +1,46 @@
-import {
-  Airplane,
-  Boat,
-} from '../03-unions-intersections/01-discriminated-unions';
+class Airplane {
+  fly() {
+    console.log("flying")
+  }
+}
+
+class Boat {
+  sail() {
+    console.log("sailing")
+  }
+}
 
 class Car {
   drive() {
-    console.log('vroom');
+    console.log("vroom")
   }
 }
 class Truck {
   tow() {
-    console.log('dragging something');
+    console.log("dragging something")
   }
 }
 
-type Vehicle = Truck | Car | Airplane | Boat;
+type Vehicle = Truck | Car | Airplane | Boat
+
+const assertNever = (value: never): never => {
+  throw new Error(`Unhandled case: ${JSON.stringify(value)}`)
+}
 
 const runVehicle = (myVehicle: Vehicle) => {
   // implement if-else or switch statements that will determine the type
+  if (myVehicle instanceof Car) {
+    myVehicle.drive();
+  } else if (myVehicle instanceof Truck) {
+    myVehicle.tow();
+  } else if (myVehicle instanceof Airplane) {
+    myVehicle.fly();
+  } else if (myVehicle instanceof Boat) {
+    myVehicle.sail();
+  } else {
+    assertNever(myVehicle);
+  }
 
   // create a final `else`-block that makes sure all possibilities are covered
   // hint: use the `never` type
-};
+}
